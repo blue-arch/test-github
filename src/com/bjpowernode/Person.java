6ab4423efe8bf282b24b0eeffcedd187a356b27a
blue-arch <1720815955@qq.com>
2021-01-04 19:22:26 +0800

    1233乱写的。

--- src/com/bjpowernode/Person.java
+++ src/com/bjpowernode/Person.java
@@ -3,6 +3,7 @@ package com.bjpowernode;
 public class Person {
     private String id;
     private String name;
+    private String address;
     public static void main(String[] args) {
         System.out.println("hello world");
 
